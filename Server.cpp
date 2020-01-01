f8703be8270569b1de65fd903ce78aeebd18ad14
yana <yanamolo96@gmail.com>
2020-01-01 22:42:31 +0200

    Revert "TEST"
    This reverts commit aad6cde2

--- Server.cpp
+++ Server.cpp
@@ -65,6 +65,14 @@ vector<float> Server::convertData(vector<string> dataVector) {
     return numVector;
 }
 
+void Server::updateTable(vector<float> values) {
+    SymbolTable *st = SymbolTable::getTable();
+    int index = 0;
+    for (float value: values) {
+        st->setVarDataBySim(simArr[index], value);
+        index++;
+    }
+}
 
 void Server::buildSimStringArray() {
     simArr[airspeed_indicator] = string("/instrumentation/airspeed-indicator/indicated-speed-kt");
